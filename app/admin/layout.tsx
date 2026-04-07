50e0d96c65f17147d307e010008aebb6e9ef1efa
Gregory Row <rowgregory@gmail.com>
2026-04-07 15:29:27 -0400

    chore(cron):Fix path to cron

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -2,6 +2,8 @@
 
 import AdminLayoutClient from '../components/pages/AdminLayoutClient'
 
+export const dynamic = 'force-dynamic'
+
 export default async function AdminLayoutPage({ children }) {
   return <AdminLayoutClient>{children}</AdminLayoutClient>
 }
