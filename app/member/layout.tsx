c8f46c5452025d34edf063b8c1fc4f0a9cc6202b
Gregory Row <rowgregory@gmail.com>
2026-04-07 15:41:26 -0400

    chore(member):Add force dynamic to member layout and remove use server

--- app/member/layout.tsx
+++ app/member/layout.tsx
@@ -1,6 +1,6 @@
-'use server'
-
 import MemberLayoutClient from '../components/pages/MemberLayoutClient'
+
+export const dynamic = 'force-dynamic'
 
 export default async function MemberLayoutPage({ children }) {
   return <MemberLayoutClient>{children}</MemberLayoutClient>
